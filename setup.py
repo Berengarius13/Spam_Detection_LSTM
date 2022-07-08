6769cb7467c8038af86aa66ba6b412cd89339d89
Piyush <78652675+Berengarius13@users.noreply.github.com>
2022-07-08 20:23:15 +0530

    setup file added

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='Spam',
+    version='',
+    packages=[''],
+    url='',
+    license='',
+    author='Piyush',
+    author_email='',
+    description=''
+)
